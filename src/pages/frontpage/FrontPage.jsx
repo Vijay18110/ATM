7a579705faa20ab5cf1fc92126377037d57c4581
Vijay18110 <vijay9628554907@gmail.com>
2024-09-19 19:56:27 +0530

    push changes

--- src/pages/frontpage/FrontPage.jsx
+++ src/pages/frontpage/FrontPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useRef, useState } from 'react'
-import styles from './FrontPage.module.css';
+
 import Buttton from '../../widgets/Buttton'
 import { generatePath, useNavigate } from 'react-router-dom';
 const FrontPage = (props) => {
@@ -30,11 +30,11 @@ const FrontPage = (props) => {
     return (
         <div className='container'>
             <div>
-                <div className={styles.inputContainer}>
-                    <input value={text} onChange={(e) => settext(e.target.value)} className={styles.input} type="tel" name="" id="" placeholder='Enter Your Card Number' />
+                <div className="inputContainer">
+                    <input value={text} onChange={(e) => settext(e.target.value)} className="input" type="tel" name="" id="" placeholder='Enter Your Card Number' />
                 </div>
                 {text.length > 0 && text.length !== 16 ? <div className='error' >Please Enter A Valid Card (16 Digits)</div> : ""}
-                <div className={styles.buttons}>
+                <div className="buttons">
                     <Buttton fn={clear} color="#1c191a" val={text.length} name="CLEAR"> </Buttton>
                     <Buttton fn={cancle} color="#a3213d" val={text.length} name="CANCLE"></Buttton>
                     <Buttton fn={fncontinue} color="#21a32e" val={text.length == 16} name="CONTINUE"></Buttton>
