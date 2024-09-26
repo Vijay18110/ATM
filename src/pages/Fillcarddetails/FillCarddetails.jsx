fbcf1f22a1f6794bb195285f0401d333cdbe9547
Vijay18110 <vijay9628554907@gmail.com>
2024-09-26 11:05:44 +0530

    changes commit

--- src/pages/Fillcarddetails/FillCarddetails.jsx
+++ src/pages/Fillcarddetails/FillCarddetails.jsx
@@ -22,54 +22,57 @@ const FillCarddetails = (props) => {
         }
     }, [users])
     const savedata = async () => {
-        const allusers = JSON.parse(localStorage.getItem('users'))
-        var user;
-        if (allusers) {
-            user = allusers.filter((item) => item.bankname === bankname);
+        // const allusers = JSON.parse(localStorage.getItem('users'))
+        // var user;
+        // if (allusers.length) {
+        //     user = allusers.filter((item) => item.bankname === bankname);
+        //     if (user.length) {
+        //         alert("you have already a ATM card for this bank. please enter another bank")
+        //         bankName.current.value = "";
+        //     }
+        // }
+
+        if (cardtype == "") {
+            alert("fill all the fields carefully")
         }
-        if (user.length) {
-            alert("you have already a ATM card for this bank. please enter another bank")
-            bankName.current.value = "";
+        else if (bankname === "") {
+
+            alert("fill all the fields carefully")
+        } else if (name === "") {
+
+            alert("fill all the fields carefully")
+        } else if (bankcard === "") {
+
+            alert("fill all the fields carefully")
+        } else if (banktype === "") {
+
+            alert("fill all the fields carefully")
+        }
+        else if (pin === "") {
+
+            alert("fill all the fields carefully")
+        } else if (number === "") {
+
+            alert("fill all the fields carefully")
+        } else if (expiry === "") {
+
+            alert("fill all the fields carefully")
+        } else if (amount === "") {
+
+            alert("fill all the fields carefully")
         }
         else {
-            if (cardtype == "") {
-                alert("fill all the fields carefully")
-            }
-            else if (bankname === "") {
-
-                alert("fill all the fields carefully")
-            } else if (name === "") {
-
-                alert("fill all the fields carefully")
-            } else if (bankcard === "") {
-
-                alert("fill all the fields carefully")
-            } else if (banktype === "") {
-
-                alert("fill all the fields carefully")
-            }
-            else if (pin === "") {
-
-                alert("fill all the fields carefully")
-            } else if (number === "") {
-
-                alert("fill all the fields carefully")
-            } else if (expiry === "") {
-
-                alert("fill all the fields carefully")
-            } else if (amount === "") {
-
-                alert("fill all the fields carefully")
-            }
-            else {
-                await setUsers([...users, { cardtype: cardtype, bankname: bankname, number: number, pin: pin, banktype: banktype, expiry: expiry, amount: amount, name: name, cardnumber: params.card, bankcard: bankcard }])
-                onhandleclick();
-            }
+            await setUsers([...users, { cardtype: cardtype, bankname: bankname, number: number, pin: pin, banktype: banktype, expiry: expiry, amount: amount, name: name, cardnumber: params.card, bankcard: bankcard }])
+            onhandleclick();
         }
+
 
     }
     const onhandleclick = () => {
         navigate('/pingenerated', { replace: true })
+    }
+    const logout = () => {
+        navigate('/', { replace: true });
     }
     return (
         <>
@@ -129,7 +132,7 @@ const FillCarddetails = (props) => {
                 </div>
                 <div className='btncard1'>
                     <button className='btn1' onClick={savedata} color="#21a32e"> save</button>
-                    <button onClick={onhandleclick} className='btn5' fn={props.logout} color="#a3213d"> back</button>
+                    <button onClick={logout} className='btn5' color="#a3213d"> back</button>
                 </div>
             </div >
         </>
