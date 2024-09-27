649e93396889a3758829cbfda6e63a35eb68cc8a
Vijay18110 <vijay9628554907@gmail.com>
2024-09-27 17:35:34 +0530

    changes commit

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import styles from './index.module.css'
+import { FaUserCircle } from "react-icons/fa";
+
 import Button2 from "../../widgets/Button2";
 import { useNavigate, useSearchParams } from "react-router-dom";
 // import { FaUserCircle } from ''
 const Profile = () => {
     const [users, setUsers] = useState([]);
+    const [value, getvalue] = useState(null);
     useEffect(() => {
         setUsers(JSON.parse(localStorage.getItem('users')))
     }, []);
@@ -13,16 +16,20 @@ const Profile = () => {
     const close = () => {
         navigate('/');
     }
+
     return <div>
         <div className={styles.Profilecont}>
             {/* <i><FaUserCircle></FaUserCircle></i> */}
             <div className="userdetail">
                 {
-                    users.length ? <>
+                    users.length ? <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                        <i>
+
+                            <FaUserCircle size="50ppx" color="white"></FaUserCircle></i>
                         <p className="phone">name: {users[0].name}</p>
                         <p className="phone">mobile: {users[0].number}</p>
                         <p style={{ color: "white" }} className="phone">totalcards: {users.length}</p>
-                    </> : ""
+                    </div> : ""
                 }
             </div>
         </div>
